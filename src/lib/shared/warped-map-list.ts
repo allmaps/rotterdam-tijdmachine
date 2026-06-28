845d171000ccd50719cd1df6e8f0c46c0391b6ba
Jules Schoonman <jules.schoonman@gmail.com>
2026-06-28 12:55:59 +0200

    catch errors

--- src/lib/shared/warped-map-list.ts
+++ src/lib/shared/warped-map-list.ts
@@ -1,8 +1,8 @@
 import { MapCollection } from '$lib/models/MapCollection';
-
 import { WarpedMapList } from '@allmaps/render';
-import type { WebGL2WarpedMap } from '@allmaps/render/webgl2';
 import { parseAnnotation } from '@allmaps/annotation';
+
+import type { WebGL2WarpedMap } from '@allmaps/render/webgl2';
 
 export const mapIdsByAnnotation = new Map<string, Set<string>>();
 export const annotationsByMapId = new Map<string, string>();
@@ -10,20 +10,30 @@ export const annotationsByMapId = new Map<string, string>();
 const collection = new MapCollection();
 const annotationUrls = collection.getAllMaps().map((map) => map.metadata.annotation);
 
-const fetchJson = (url: string) => fetch(url).then((resp) => resp.json());
-
+// Fetch and parse annotations
 const georeferencedMaps = await Promise.all(
-	annotationUrls.map((url) =>
-		fetchJson(url).then((data) => {
-			const parsedAnnotations = parseAnnotation(data);
-			const ids = parsedAnnotations.flatMap(({ id }) => (id ? [id] : []));
-			mapIdsByAnnotation.set(url, new Set(ids));
-			ids.forEach((ids) => annotationsByMapId.set(ids, url));
-			return parsedAnnotations;
-		})
-	)
+	annotationUrls.map(async (url) => {
+		try {
+			const resp = await fetch(url);
+			if (resp.ok) {
+				const data = await resp.json();
+				const parsedAnnotations = parseAnnotation(data);
+				const ids = parsedAnnotations.flatMap(({ id }) => (id ? [id] : []));
+				mapIdsByAnnotation.set(url, new Set(ids));
+				ids.forEach((ids) => annotationsByMapId.set(ids, url));
+				return parsedAnnotations;
+			} else {
+				console.log('Fetch failed for', url, resp);
+				return [];
+			}
+		} catch {
+			console.log('Fetch failed for', url);
+			return [];
+		}
+	})
 );
 
+// Function to create a warpedMapList with maps added
 export const getWarpedMapList = () => {
 	const warpedMapList = new WarpedMapList<WebGL2WarpedMap>();
 	warpedMapList.addGeoreferencedMaps(georeferencedMaps.flat());
